a2cc4c1177e459326613c86ea3c1f2ad595c4058
Josephine Strange <jostra@microsoft.com>
2019-01-23 10:36:42 -0800

    first commit

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+var connection = require("connection.js");
+
+
+// Config Setup
+
+
+// Inside your burger directory, create a folder named config.
+// Create a connection.js file inside config directory.
+
+
+
+// Inside the connection.js file, setup the code to connect Node to MySQL.
+// Export the connection.
+
+
+
+// Create an orm.js file inside config directory.
+
+
+
+// Import (require) connection.js into orm.js
+
+// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+
+// Export the ORM object in module.exports.
+
+
+
+// Model setup
+
+
+module.exports = connectionToSql;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+ 
+INSERT INTO menu (id, burger_name, devoured) VALUES ('101','veggie_burger', '0');
+INSERT INTO menu (id, burger_name, devoured) VALUES ('102', 'cheeseburger', '0');
+INSERT INTO menu (id, burger_name, devoured) VALUES ('103', 'turkeyburger', '0');
+INSERT INTO menu (id, burger_name, devoured) VALUES ('104', 'falafalburger', '0');
+INSERT INTO menu (id, burger_name, devoured) VALUES ('105', 'chickenburger', '0');
